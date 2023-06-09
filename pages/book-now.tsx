6d53db1f42847a792ba0a9c1ff1230061c5e572b
Daniel Coder <danielp.coder93@gmail.com>
2023-06-09 13:32:46 -0400

    fixed font for vercel and added some padding for book-now page

--- pages/book-now.tsx
+++ pages/book-now.tsx
@@ -1,10 +1,17 @@
 import CalendlyForm from "../components/CalendlyForm";
 import Section from "../components/Section";
-
+import classes from "../styles/Calendly.module.scss";
 export default function Home() {
   return (
     <Section bg="light">
-      <h1>Thank you!</h1> <CalendlyForm />
+      <div className={classes.textAlign}>
+        <h1 className={classes.padding}>Book Now.</h1>
+        <h3 style={{ color: "rgb(104,104,104)" }}>
+          Skip the wait and book your discovery call with our co-founder, Dan
+          Coder now.
+        </h3>{" "}
+      </div>{" "}
+      <CalendlyForm />
     </Section>
   );
 }
